e1b8496ebee258c60bf2a28295f7a7dc87968394
Akshaya Sree <akshayasreem@gmail.com>
2023-01-05 18:25:24 +0530

    version 2 changes

--- js/custom.js
+++ js/custom.js
@@ -93,6 +93,20 @@ $(document).ready(function () {
     //         $(".navbar-fixed-top").removeClass("bg-nav");
     //     }
     // });
+    jQuery(document).ready(function($) {
+      // Scroll to the desired section on click
+      // Make sure to add the `data-scroll` attribute to your `<a>` tag.
+      // Example: 
+      // `<a data-scroll href="#my-section">My Section</a>` will scroll to an element with the id of 'my-section'.
+      function scrollToSection(event) {
+        event.preventDefault();
+        var $section = $($(this).attr('href')); 
+        $('html, body').animate({
+          scrollTop: $section.offset().top
+        }, 1000);
+      }
+      $('[data-scroll]').on('click', scrollToSection);
+    }(jQuery));
 
     // new header 1
     const hamburger = document.querySelector(".hamburger");
@@ -112,6 +126,7 @@ hamburger.addEventListener('click', ()=>{
     //Hamburger Animation
     hamburger.classList.toggle("toggle");
 });
+
 
 
  //scroll to top starts
